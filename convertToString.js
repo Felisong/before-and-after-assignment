ca8c5d22c1ee8759ac1910e498d171188ef99ee0
Felisong <carolinahs100@gmail.com>
2024-11-06 10:08:42 -0700

    converted a number to a string, i used typeof to tell which one it is.

---
+++ convertToString.js
@@ -0,0 +1,10 @@
+/* I want to make a number to a string 
+So i need a num variable, i have found the function is .toString()
+i need to show a normal number in the before, then a string after.
+*/
+let num = 15;
+console.log(`Before: ${num} is a ${typeof num}`);
+
+num = num.toString();
+
+console.log(`After: ${num} is a ${typeof num}`);
